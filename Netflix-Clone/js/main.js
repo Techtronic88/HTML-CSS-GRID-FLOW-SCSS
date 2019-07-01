186d95a69037814afca82d38016f3f348584170a
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-07-01 23:54:05 +1000

    Add files via upload
    - Added functionality to Javascript where we can click on the tabs and have red borders applied.

--- Netflix-Clone/js/main.js
+++ Netflix-Clone/js/main.js
@@ -1,6 +1,22 @@
-const tabItems = document.querySelector(".tab-item");
+// BECAREFUL: using document.querySelecTor in this case WILL NOT give you a nodeList to iterate over
+// This is why we need to use document.querySelectorAll.
+const tabItems = document.querySelectorAll(".tab-item");
 const tabContentItem = document.querySelector(".tab-content-item");
 
-function selectItem(e) {}
+// Adding on Red Bottom Border to each tabs we click on
+function selectItem(e) {
+  removeBorders();
+  this.classList.add("tab-border");
+}
 
-tabItems.forEach(item => item.addEventListener("click", selectItem));
+// Remove borders from tabs after each items clicked on
+// NOTE: function declaration is hoisted to the top.
+function removeBorders(e) {
+  tabItems.forEach(item => {
+    item.classList.remove("tab-border");
+  });
+}
+
+tabItems.forEach(item => {
+  item.addEventListener("click", selectItem);
+});
