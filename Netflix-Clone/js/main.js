dc07b38c5a3ee2201481ce5792d6060593d4f336
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-07-04 23:31:50 +1000

    Add files via upload
    -Added in completed JS for this mini app.

--- Netflix-Clone/js/main.js
+++ Netflix-Clone/js/main.js
@@ -1,12 +1,19 @@
 // BECAREFUL: using document.querySelecTor in this case WILL NOT give you a nodeList to iterate over
 // This is why we need to use document.querySelectorAll.
 const tabItems = document.querySelectorAll(".tab-item");
-const tabContentItem = document.querySelector(".tab-content-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 // Adding on Red Bottom Border to each tabs we click on
 function selectItem(e) {
   removeBorders();
+  removeShow();
   this.classList.add("tab-border");
+
+  // Grab all contained elements inside of the tabs
+  // Add it into the existing DOM dynamiclly
+  const gettingItems = document.querySelector(`#${this.id}-content`);
+  gettingItems.classList.add("show");
+  console.log(this.id);
 }
 
 // Remove borders from tabs after each items clicked on
@@ -14,6 +21,13 @@ function selectItem(e) {
 function removeBorders(e) {
   tabItems.forEach(item => {
     item.classList.remove("tab-border");
+  });
+}
+
+// We have to remove any existing elements from the DOM
+function removeShow() {
+  tabContentItems.forEach(item => {
+    item.classList.remove("show");
   });
 }
 
